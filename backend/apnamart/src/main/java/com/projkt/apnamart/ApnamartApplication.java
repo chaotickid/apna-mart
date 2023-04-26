076bf1f5277200328b161bd091d388b0c72c8a0e
Aditya Patil <aditya.patil@mavenir.com>
2023-04-26 09:28:57 +0530

    Initial commit

---
+++ backend/apnamart/src/main/java/com/projkt/apnamart/ApnamartApplication.java
@@ -0,0 +1,13 @@
+package com.projkt.apnamart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApnamartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApnamartApplication.class, args);
+	}
+
+}
---
+++ backend/apnamart/src/test/java/com/projkt/apnamart/ApnamartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projkt.apnamart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApnamartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
